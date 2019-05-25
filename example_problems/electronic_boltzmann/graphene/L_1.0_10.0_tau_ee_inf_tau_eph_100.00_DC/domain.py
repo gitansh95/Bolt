a2500d2d3f7e271422975050d0fbf5233a88df52
gitansh95 <gitansh95@gmail.com>
2019-05-25 08:45:28 -0400

    Added high aspect ratio graphene strip to examples

---
+++ example_problems/electronic_boltzmann/graphene/L_1.0_10.0_tau_ee_inf_tau_eph_100.00_DC/domain.py
@@ -0,0 +1,40 @@
+q1_start = 0.
+q1_end   = 1.0
+N_q1     = 72
+
+q2_start = 0.
+q2_end   = 10.0
+N_q2     = 720
+
+# If N_p1 > 1, mirror boundary conditions require p1 to be
+# symmetric about zero
+
+# In the polar representation of momentum space,
+# p1 = p_r (magnitude of momentum)
+# Here, p1_start and p1_end have been adjusted such that
+# p_r_center is 1.0
+
+## TODO: REMOVE HARDCODED VALUES, FIGURE OUT HOW TO IMPORT LIBRARIES IN DOMAIN
+p1_start = 0.015 - 64.*12e-5
+p1_end   = 0.015 + 64.*12e-5
+N_p1     = 64
+
+# If N_p2 > 1, mirror boundary conditions require p2 to be
+# symmetric about zero
+
+# In the polar representation of momentum space,
+# p2 = p_theta (angle of momentum)
+# N_p_theta MUST be even.
+
+p2_start =  -3.14159265359
+p2_end   =  3.14159265359
+N_p2     =  512
+
+# If N_p3 > 1, mirror boundary conditions require p3 to be
+# symmetric about zero
+p3_start = -0.5
+p3_end   =  0.5
+N_p3     =  1
+
+N_ghost = 2
+
